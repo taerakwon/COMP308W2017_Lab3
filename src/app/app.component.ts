eae63f56697b920c21207698d2a3f0ca6102ae01
taerakwon <taerakwon@gmail.com>
2017-03-11 19:51:56 -0500

    Divided Heroes and Details into components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,7 @@ const HEROES: Hero[] = [
       <span class="badge">{{hero.id}}</span> {{hero.name}}
     </li>
   </ul>
+  <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
   styles: [`
   .selected {
