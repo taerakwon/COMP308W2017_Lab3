fdf2da289fa07b7db17dd47b4b3569c95ffa2e50
taerakwon <taerakwon@gmail.com>
2017-03-11 19:34:49 -0500

    Added styling to selected hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,9 @@ import { Component } from '@angular/core';
   <h1>{{title}}</h1>
   <h2>My Heroes</h2>
   <ul class="heroes">
-    <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+    <li *ngFor="let hero of heroes" 
+      [class.selected]="hero == selectedHero"
+      (click)="onSelect(hero)">
       <span class="badge">{{hero.id}}</span> {{hero.name}}
     </li>
   </ul>
@@ -23,7 +25,7 @@ import { Component } from '@angular/core';
   styles: [`
   .selected {
     background-color: #CFD8DC !important;
-    color: white;
+    color: white;    
   }
   .heroes {
     margin: 0 0 2em 0;
